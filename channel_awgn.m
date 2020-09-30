07b4a783e6997a288ebf022bd1e3434cdc76eb0c
Aditya Jain <ajain093@gmail.com>
2020-09-30 22:39:30 +0530

    Update channel_awgn.m

--- channel_awgn.m
+++ channel_awgn.m
@@ -1,6 +1,6 @@
 function [awgn_data] = channel_awgn(tx_data,sgma)
 
-inoise = randn(1,length(tx_data)).*sgma;   %Real Part of Noise
+inoise = randn(1,length(tx_data)).*sgma;   % Noise Generated through randn
 
 awgn_data = tx_data + inoise; 
 
